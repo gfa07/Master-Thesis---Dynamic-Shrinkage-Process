48d2f3ed769febd12eca438ea5cbc7d9abe85900
Gonçalo Albino <goal7116@student.su.se>
2026-03-31 10:45:25 +0200

    Update SimulatedData.R

--- data/SimulatedData.R
+++ data/SimulatedData.R
@@ -9,11 +9,11 @@ for (t in 1:T){
   betas[t,2] = if (t/T < 0.5) -0.5 else 0.5
   betas[t,3] = (t/T)^2
 }
-par(mfrow = c(2,2), oma = c(0, 0, 3, 0))
+par(mfrow = c(2,2))
 for (j in 1:P){
   plot(betas[,j], type = "l", xlab = 'Time', ylab = bquote(beta[.(j)]))
 }
-mtext("Evolution of the coefficients", outer = T, cex = 1.5, font = 2)
+
 
 X <- matrix(rnorm(T*(P-1), mean = 0, sd = 1), nrow = T, ncol = P-1)
 X <- cbind('Intercept' = 1, X)
