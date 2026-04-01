644e95d14e495d1d2718252bdff39aecf1f52f3e
Gonçalo Albino <goal7116@student.su.se>
2026-04-01 11:54:19 +0200

    Add files via upload

---
+++ R/ApproximationXiDist..R
@@ -0,0 +1,31 @@
+#Approximate PG - Xi
+
+library('BayesLogit')
+prettycol = c("#6C8EBF", "#c0a34d", "#780000","#007878","#B5C6DF","#EADAAA","#AE6666")
+set.seed(123)
+
+n = 10000
+b = 1
+c = 0
+lim = c(0,4)
+
+xi = rpg(num = n, h = b, z = c)
+
+hist(xi, 30, prob = TRUE, col = prettycol[6],
+     lwd = 0.5, ylim=lim, main = '', ylab = 'Density', xlab = expression('Draws of' ~ xi))
+
+dens = density(xi)
+lines(x=dens$x, y=dens$y, col = prettycol[1], lwd = 2)
+
+x = seq(from = 0, to = 2, length.out = 1000)
+mu = mean(log(xi))
+sigma = sd(log(xi))
+densLogNorm = dlnorm(x, meanlog = mu, sdlog = sigma)
+lines(x=x, y = densLogNorm, col = prettycol[3], lwd = 2 )
+
+legend(x = 'topright', legend = c('Pólya-Gamma Distribution', 'Log-Normal Distribution'), lwd = c(2,2), 
+       lty=c(1,1),col= c(prettycol[1],prettycol[3]), cex = 0.75)
+
+mu
+sigma
+
