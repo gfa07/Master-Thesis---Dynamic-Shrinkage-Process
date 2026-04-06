f7c60830b4f57b30ea603410ee6d6d1a15b7e534
Gonçalo Albino <goal7116@student.su.se>
2026-04-06 11:44:38 +0200

    Update SimulatedData.R

--- data/SimulatedData.R
+++ data/SimulatedData.R
@@ -9,11 +9,16 @@ for (t in 1:T){
   betas[t,2] = if (t/T < 0.5) -0.5 else 0.5
   betas[t,3] = (t/T)^2
 }
-par(mfrow = c(2,2))
-for (j in 1:P){
-  plot(betas[,j], type = "l", xlab = 'Time', ylab = bquote(beta[.(j)]))
-}
 
+cairo_pdf("betas_plot.pdf", width = 7, height = 3)
+
+par(mfrow = c(1,3), mar = c(4.5, 5, 2, 1), mgp = c(2.5, 1, 0)) 
+
+for (j in 1:P){
+  plot(betas[,j], type = "l", lwd = 2, xlab = 'TimeStep', ylab = bquote(beta[.(j)]),
+       cex.lab = 1.4, cex.axis = 1.2)
+}
+dev.off()
 
 X <- matrix(rnorm(T*(P-1), mean = 0, sd = 1), nrow = T, ncol = P-1)
 X <- cbind('Intercept' = 1, X)
